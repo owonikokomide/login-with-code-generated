9268b75d14de6a078c033c0f25833bbd0dafb189
owonikoko Ayomide <donaykoko@yahoo.com>
2023-11-14 12:36:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="login.css"> 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>sign up</title>
