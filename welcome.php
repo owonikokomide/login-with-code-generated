cf18e873970ec7345eb4cb3fb6a420d16e5db67e
owonikoko Ayomide <donaykoko@yahoo.com>
2024-06-12 10:47:32 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="login.css"> 
+  <link rel="stylesheet" href="login.css">  
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   <title>Welcome</title>
